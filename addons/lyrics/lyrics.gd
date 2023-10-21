4abceb690b052a84029f77602e8ba7ff648e8340
Breakdown08 <41376576+Breakdown08@users.noreply.github.com>
2023-10-21 16:46:52 +0700

    android

--- addons/lyrics/lyrics.gd
+++ addons/lyrics/lyrics.gd
@@ -3,6 +3,7 @@ class_name Lyrics
 
 var chords_positions: Dictionary = {}
 @export var chord_offset: Vector2 = Vector2(-5, -40)
+@export var chord_node = preload("res://addons/song_chord/song_chord.tscn")
 	
 	
 func set_caret_position(column: int, line: int):
@@ -16,7 +17,8 @@ func add_chords(tabs_data: Array[Dictionary]):
 
 
 func register_new_chord(chord_name: String, line: int, column: int):
-	var chord: SongChord = SongChord.new()
+	#var chord: SongChord = SongChord.new()
+	var chord = chord_node.instantiate()
 	chord.lyrics = self
 	chords_positions.merge({chord: {"name": chord_name, "line": line, "column": column}})
 	add_child(chord)
